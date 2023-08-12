d9f9665385e52bb2afecc0bc80b46081bcf53fff
kyleSong <107303776+Songssh@users.noreply.github.com>
2023-08-12 15:34:03 +0900

    Update ipmi_setting.bat

--- ipmi_setting.bat
+++ ipmi_setting.bat
@@ -1,9 +1,12 @@
 @echo off
 
+rem edit your ipmitoll's path
 cd c:/ipmitool
 
+rem edit ip, id, password
 ipmitool -I lanplus -H 192.168.0.220 -U root -P calvin raw 0x30 0x30 0x01 0x00
 echo manual fan control activated
 
+rem edit ip, id, password
 ipmitool -I lanplus -H 192.168.0.220 -U root -P calvin raw 0x30 0x30 0x02 0xff %1
 echo success
